1dcd77eb721b7f807a4bdf9c1bed57de556f8810
ashish358 <ashishdhadekar358@gmail.com>
2026-02-08 00:34:48 +0530

    debug: log VITE_API_URL

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App";
 import { CartProvider } from "./context/CartContext";
 import "./index.css";
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CartProvider>
